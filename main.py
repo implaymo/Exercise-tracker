a1cc517fc7900bc5d6c42571a2c522f20559ebb6
implaymo <136201112+implaymo@users.noreply.github.com>
2023-11-07 20:42:12 +0000

    Finished project.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from dotenv import load_dotenv
 import os
+from datetime import datetime
 def configure():
     load_dotenv()
 
@@ -8,9 +9,15 @@ configure()
 
 APP_ID = os.getenv("APP_ID")
 APP_KEY= os.getenv("APP_KEY")
-EXERCISE_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+MY_EMAIL = os.getenv("MY_EMAIL")
+EXERCISE_ENDPOINT = os.getenv("EXERCISE_ENDPOINT")
+POST_ENDPOINT = os.getenv("POST_ENDPOINT")
 
+today = datetime.now()
+today_date = today.strftime("%d/%m/%Y")
+time = today.strftime("%X")
 
+# Nutritionix.com Exercise Enpoint
 headers = {
     "x-app-id": APP_ID,
     "x-app-key": APP_KEY,
@@ -25,8 +32,26 @@ exercise_data = {
     "age": 26,
 }
 
-response = requests.post(url=EXERCISE_ENDPOINT, headers=headers, json=exercise_data)
-response.raise_for_status()
+response_nutri = requests.post(url=EXERCISE_ENDPOINT, headers=headers, json=exercise_data)
+response_nutri.raise_for_status()
 
-data = response.json()
-print(data)
+data = response_nutri.json()
+
+
+# Sheety.com Endpoint
+for exercise in data:
+
+data_for_sheet = {
+    "sheet1":
+        {
+            "date": today_date,
+            "time": time,
+            "exercise": data["exercises"][0]["user_input"],
+            "duration": data["exercises"][0]["duration_min"],
+            "calories": data["exercises"][0]["nf_calories"]
+
+        }
+}
+
+response_sheety = requests.post(url=POST_ENDPOINT, json=data_for_sheet, headers=headers)
+response_sheety.raise_for_status()
